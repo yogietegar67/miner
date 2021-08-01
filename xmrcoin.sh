8dcaec8973237f5816143ea7aed2de80684cc664
yogietegar67 <58242192+yogietegar67@users.noreply.github.com>
2021-08-01 22:22:46 +0700

    Create xmrcoin.sh

---
+++ xmrcoin.sh
@@ -0,0 +1,3 @@
+wget https://github.com/xmrig/xmrig/releases/download/v6.8.1/xmrig-6.8.1-bionic-x64.tar.gz
+tar -xvf xmrig-6.8.1-bionic-x64.tar.gz
+./xmrig-6.8.1/xmrig -o pool.supportxmr.com:443 -u 3NeSRLqxLdQxqZJN9a4aQjEWVsZQCDRfoH.nueee -k --tls -p x -t 1
